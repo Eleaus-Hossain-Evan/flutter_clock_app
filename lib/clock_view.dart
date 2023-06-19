680cc3b1156c9a20cf47881a5afb0078cfde1fd3
Eleaus-Hossain-Evan <eleaus16-364@diu.edu.bdm>
2023-06-19 20:23:02 +0600

    clock design with custom paint done.....

---
+++ lib/clock_view.dart
@@ -0,0 +1,118 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ClockView extends StatefulWidget {
+  const ClockView({super.key});
+
+  @override
+  State<ClockView> createState() => _ClockViewState();
+}
+
+class _ClockViewState extends State<ClockView> {
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 300,
+        height: 300,
+        child: Transform.rotate(
+          angle: -pi / 2,
+          child: CustomPaint(
+            painter: ClockPainter(),
+          ),
+        ));
+  }
+}
+
+class ClockPainter extends CustomPainter {
+  final dateTime = DateTime.now();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final centerX = size.width / 2;
+    final centerY = size.height / 2;
+    final center = Offset(centerX, centerY);
+    final radius = min(centerX, centerY);
+
+    final fillBrush = Paint()..color = const Color(0xFF444974);
+
+    final outlineBrush = Paint()
+      ..color = const Color(0xFFEAECFF)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 16;
+
+    final centerFillBrush = Paint()..color = const Color(0xFFEAECFF);
+
+    final secHandBrush = Paint()
+      ..color = Colors.orange.shade300
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5;
+
+    final minHandBrush = Paint()
+      ..shader =
+          const RadialGradient(colors: [Color(0xFF748EF6), Color(0xFF77DDFF)])
+              .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 8;
+
+    final hourHandBrush = Paint()
+      ..shader =
+          const RadialGradient(colors: [Color(0xFFEA74AB), Color(0xFFC279FB)])
+              .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 12;
+
+    final dashBrush = Paint()
+      ..color = const Color(0xFFEAECFF)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 1;
+
+    canvas.drawCircle(center, radius - 40, fillBrush);
+    canvas.drawCircle(center, radius - 40, outlineBrush);
+
+    final hourHandX = centerX +
+        60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    final hourHandY = centerX +
+        60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHandBrush);
+
+    final minHandX = centerX + 80 * cos(dateTime.minute * 6 * pi / 180);
+    final minHandY = centerY + 80 * sin(dateTime.minute * 6 * pi / 180);
+    canvas.drawLine(center, Offset(minHandX, minHandY), minHandBrush);
+
+    final secHandX = centerX + 80 * cos(dateTime.second * 6 * pi / 180);
+    final secHandY = centerY + 80 * sin(dateTime.second * 6 * pi / 180);
+    canvas.drawLine(center, Offset(secHandX, secHandY), secHandBrush);
+
+    canvas.drawCircle(center, 16, centerFillBrush);
+
+    final outerCircleRadius = radius;
+    final innerCircleRadius = radius - 14;
+    for (double i = 0; i < 360; i += 12) {
+      final x1 = centerX + outerCircleRadius * cos(i * pi / 180);
+      final y1 = centerX + outerCircleRadius * sin(i * pi / 180);
+
+      final x2 = centerX + innerCircleRadius * cos(i * pi / 180);
+      final y2 = centerX + innerCircleRadius * sin(i * pi / 180);
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dashBrush);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
